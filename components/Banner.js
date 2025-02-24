c56ba0b524972fbf0faafa3b813b15e6e1b4bf64
root <root@Etienne.>
2025-02-24 22:16:32 +0100

    MAJ 24 5

--- components/Banner.js
+++ components/Banner.js
@@ -2,11 +2,12 @@
 import React from "react";
 
 const Banner = ({ title, backgroundImage }) => {
+  const style = backgroundImage
+    ? { backgroundImage: `url(${backgroundImage})`, backgroundPosition: "50% 30%" }
+    : {};
+
   return (
-    <div
-      className="w-full h-64 relative bg-cover bg-center"
-      style={{ backgroundImage: `url(${backgroundImage})`, backgroundPosition: "50% 30%" }}
-    >
+    <div className="w-full h-64 relative bg-cover bg-center" style={style}>
       <h1 className="relative text-4xl md:text-5xl font-bold text-blue-500 text-center pt-20">
         {title}
       </h1>
